f072e7c9dd0a7d8bcaf6752dd3e814fbb26c05b8
AndreaVk <vico.ac1996@gmail.com>
2022-02-09 19:04:30 +0100

    resource

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -12,6 +12,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config("comics");
+
+        foreach($comics as $item) {
+            $newComic = new Comic();
+            $newComic->title = $item["title"];
+            $newComic->description = $item["description"];
+            $newComic->series = $item["series"];
+            $newComic->thumb = $item["thumb"];
+            $newComic->sale_date = $item["sale_date"];
+            $newComic->type = $item["type"];
+            $newComic->price = $item["price"];
+            $newComic->save();
+
+        }
     }
 }
